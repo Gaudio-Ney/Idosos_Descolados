f02438d15634eef860f60ab5885c6c9e4f93383e
Gáudio Uchôa Ney <gaudio.ney@gmail.com>
2020-04-03 15:06:50 -0300

    Projeto XCode

---
+++ Idosos_Descolados_XCode/Idosos_Descolados_XCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Idosos_Descolados_XCode
+//
+//  Created by Gáudio Ney on 03/04/20.
+//  Copyright © 2020 Gáudio Ney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
