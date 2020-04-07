eb713842dc3f101077f42c5880b3fdc563147cc4
_salinasjo <veiga.salinas@gmail.com>
2020-04-07 14:46:35 -0300

    Parte.v2.Perfil
    Parte do perfil incompleta

---
+++ Idosos_Descolados_XCode/Idosos_Descolados_XCode/Perfil_ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  Perfil_ViewController.swift
+//  Idosos_Descolados_XCode
+//
+//  Created by joana veiga salinas vega on 06/04/20.
+//  Copyright © 2020 Gáudio Ney. All rights reserved.
+//
+
+import UIKit
+
+class Perfil_ViewController: UIViewController {
+
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var age: UITextField!
+    
+    @IBOutlet weak var adress: UITextField!
+    
+    @IBOutlet weak var profession: UITextField!
+    
+    @IBOutlet weak var myinterest: UITextField!
+    
+    @IBOutlet weak var regularyExercise: UITextField!
+
+    
+    @IBOutlet weak var history: UILabel!
+    
+    
+    @IBOutlet weak var saveChanges: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        
+        name.text
+        
+        
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after
+        //loading the view.
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
